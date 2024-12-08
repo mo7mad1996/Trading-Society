8e99a2e46c6f2d7824350b93d8b757b6daf5c58f
Mohamed Ibrahim <mo7mad369@gmail.com>
2024-12-08 22:39:14 +0200

    debuging...

--- src/pages/Dashboard/RecordedSessonsPage/index.jsx
+++ src/pages/Dashboard/RecordedSessonsPage/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
 
-import OnlineSessionCard from "./Onlinesessioncard.jsx";
+import Onlinesessioncard from "./Onlinesessioncard.jsx";
 
 const RecordedSessionsPage = () => {
   return (
@@ -26,7 +26,7 @@ const RecordedSessionsPage = () => {
           }}
         ></Box>
       </Box>
-      <OnlineSessionCard />
+      <Onlinesessioncard />
     </Box>
   );
 };
--- src/pages/Dashboard/RecordedSessonsPage/onlinesessioncard.jsx
+++ src/pages/Dashboard/RecordedSessonsPage/onlinesessioncard.jsx
@@ -10,7 +10,7 @@ import {
 import { useNavigate } from "react-router-dom"; // Import useNavigate from react-router-dom
 import useApi from "@/api";
 
-function OnlineSessionCard() {
+function Onlinesessioncard() {
   // config
   const navigate = useNavigate();
   const api = useApi();
@@ -126,4 +126,4 @@ function OnlineSessionCard() {
   );
 }
 
-export default OnlineSessionCard;
+export default Onlinesessioncard;
