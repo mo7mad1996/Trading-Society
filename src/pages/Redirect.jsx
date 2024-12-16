238f8aa231a9818d8d88553a82d53adfea051ab7
Mohamed Ibrahim <mo7mad369@gmail.com>
2024-12-17 01:13:54 +0200

    edit redirect to TS...

--- src/pages/Redirect.jsx
+++ src/pages/Redirect.jsx
@@ -6,28 +6,28 @@ import { useNavigate } from "react-router-dom";
 
 export default function Redirect() {
   const [token] = useSearchParams();
+  // const api = useApi();
 
   const { setUser } = useContext(TokenContext);
   let navigate = useNavigate();
 
-  const getUserInfo = async () => {
-    const api = useApi();
+  // const getUserInfo = async () => {
+  //   try {
+  //     const res = await api.get("/user");
+  //     const data = res.data.user;
 
-    try {
-      const res = await api.get("/user");
-      const data = res.data.user;
-
-      setUser(data);
-      navigate("/home");
-    } catch (err) {
-      navigate("/login");
-    }
-  };
+  //     setUser(data);
+  //   } catch (err) {
+  //     navigate("/login");
+  //   }
+  // };
 
   useEffect(() => {
     localStorage.setItem("token", token);
 
-    getUserInfo();
+    if (token) navigate("/home");
+    else navigate("/login");
+    // getUserInfo();
   }, []);
 
   return <div>Loading...</div>;
