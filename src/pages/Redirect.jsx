bc91c1901242a3822d0643231f472b9971bcd1e4
Mohamed Ibrahim <mo7mad369@gmail.com>
2024-12-17 20:41:42 +0200

    edit redirect.

--- src/pages/Redirect.jsx
+++ src/pages/Redirect.jsx
@@ -1,34 +1,19 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import useApi from "@/api";
 import { useContext, useEffect } from "react";
 import { TokenContext } from "@/context";
-import { useNavigate } from "react-router-dom";
+import {} from "react-router-dom";
 
 export default function Redirect() {
-  const [token] = useSearchParams();
-  // const api = useApi();
-
-  const { setUser } = useContext(TokenContext);
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token");
   let navigate = useNavigate();
-
-  // const getUserInfo = async () => {
-  //   try {
-  //     const res = await api.get("/user");
-  //     const data = res.data.user;
-
-  //     setUser(data);
-  //   } catch (err) {
-  //     navigate("/login");
-  //   }
-  // };
 
   useEffect(() => {
     localStorage.setItem("token", token);
-
     if (token) navigate("/home");
     else navigate("/login");
-    // getUserInfo();
   }, []);
 
-  return <div>Loading...</div>;
+  return <div>Redirect...</div>;
 }
